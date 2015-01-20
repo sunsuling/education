367a6ab8da526d451e67cffe866edcea102a66ee
sunsuling <suling_sun@163.com>
2015-01-20 20:33:45 +0800

    前端页面

--- app/Global.java
+++ app/Global.java
@@ -37,14 +37,14 @@ public class Global extends GlobalSettings {
                 String pattern = "onRequest  clientAddress = {0}，path = {1}";
                 Logger.info(MessageFormat.format(pattern, clientAddress, path));
 
-                if (path.matches("/education/.*") && !path.matches("/education/admin/dologin")
-                        && !path.matches("/education/admin/doregister")
-                        && !path.matches("/education/admin/register")){
-                    UserSession userSession = UserSession.getCurrent(context.session());
-                    if (userSession == null) {
-                        return F.Promise.pure(redirect("/"));
-                    }
-                }
+//                if (path.matches("/education/.*") && !path.matches("/education/admin/dologin")
+//                        && !path.matches("/education/admin/doregister")
+//                        && !path.matches("/education/admin/register")){
+//                    UserSession userSession = UserSession.getCurrent(context.session());
+//                    if (userSession == null) {
+//                        return F.Promise.pure(redirect("/"));
+//                    }
+//                }
 
                 F.Promise callPoint = delegate.call(context);
                 return callPoint;
@@ -57,11 +57,11 @@ public class Global extends GlobalSettings {
     @Override
     public F.Promise<Result> onHandlerNotFound(Http.RequestHeader requestHeader) {
         Logger.info("=======onHandlerNotFound===============");
-        Http.Context context = Http.Context.current();
-        UserSession userSession = UserSession.getCurrent(context.session());
-        if (userSession == null) {
-            return F.Promise.pure(redirect("/"));
-        }
+//        Http.Context context = Http.Context.current();
+//        UserSession userSession = UserSession.getCurrent(context.session());
+//        if (userSession == null) {
+//            return F.Promise.pure(redirect("/"));
+//        }
         return super.onHandlerNotFound(requestHeader);
     }
 }
